081534a9e2131b001fa5c17a30edc2cf5ad92d72
00Fil <filippo.corsini.xyz@gmail.com>
2026-05-28 01:37:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,27 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Volo tra le righe | Playlist",
-  description:
-    "Elaborato playlist ispirato a Il gioco della salamandra di Davide Longo.",
+	title: "Volo tra le righe",
+	description: "Playlist narrativa — Il gioco della salamandra",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="it">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-        	href="https://fonts.googleapis.com/css2?family=Borel&family=Space+Grotesk:wght@700&display=swap"
-        	rel="stylesheet"
-        />
-        <head/>
-      <body>{children}</body>
-    </html>
-  );
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="it">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Borel&family=Space+Grotesk:wght@700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
